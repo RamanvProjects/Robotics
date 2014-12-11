2b129cc94b37638ea4b869cb0cb6b766b50d83e8
Vivek Ramanujan <ramanv@robosalmon.(none)>
2014-12-10 18:32:26 -0800

    added listener, still need to work on publishing to node (does not connect to gazebo)

--- pyTest.py
+++ pyTest.py
@@ -5,38 +5,20 @@ from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Twist
-
-
-cmd = rospy.Publisher("/atrv/motion", Twist)
-motion = Twist()
-def callback(msg):
-    position = msg.pose.position
-        if position.x < 1:
-            motion.linear.x = +0.5
-        if position.x > 2:
-            motion.linear.x = -0.5
-    cmd.publish(motion)
-
-rospy.init_node("rostuto1")
-rospy.Subscriber("/atrv/pose", PoseStamped, callback)
-rospy.spin() # this will block untill you hit Ctrl+C
-
-
+from std_msgs.msg import ColorRGBA
 file = open('test.txt', 'w')
+
 def main():
 	rospy.init_node('sub_pcl');
 
-	processor = Processor(rospy.Time.now())
+	processor = Processor()
 	rospy.Subscriber('/camera/depth/points', PointCloud2, processor.callback, queue_size=1)
 	rospy.spin()
 
 class Processor(object):
 	"""Computes and logs the time taken to process a point cloud."""
-	def __init__(self, prev_time):
-		self._prev_time = prev_time
-		self._time_taken = rospy.Duration(0)
-		self._num_calls = 0
-
+	def __init__(self):
+		self.pub = rospy.Publisher('chatter_color', ColorRGBA)
 
 	def callback(self, data):
 		"""Computes the average point in a point cloud and saves timing info."""
@@ -53,23 +35,51 @@ class Processor(object):
 			avg_y += y
 			avg_z += z
 
+	
 		if num_points > 0:
 			avg_x /= num_points
 			avg_y /= num_points
 			avg_z /= num_points
 
-		current_time = rospy.Time.now()
-		self._time_taken += current_time - self._prev_time
-		self._prev_time = current_time
-		self._num_calls += 1
+		self.pub.publish(avg_x,avg_y,avg_z,0)
 
-		rospy.loginfo('x: {}, y: {}, z: {}, time/point: {}s'.format(
-			avg_x, avg_y, avg_z, self._time_taken.to_sec() / self._num_calls))
 
-		file.write("distance: ")
-		distance = (avg_x**2 + avg_y**2 + avg_z**2)**.5
-		file.write(str(distance))
-		file.write("\n")
-		
+
+	
+class Mover():
+	"""Moves based on published data"""
+	def __init__(self):
+		super(Mover, self).__init__()
+		self.cmd = rospy.Publisher("/base_controller/command", Twist)
+		self.motion = Twist()
+
+	def callback(self, msg):
+		#position = msg.pose.position
+		self.motion.linear.x = vector[0]
+		self.motion.linear.z = vector[2]
+
+		print("here")
+		self.cmd.publish(motion)	
 if __name__ == '__main__':
 	main()
+
+'''
+import rospy
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
+
+cmd = rospy.Publisher("/atrv/motion", Twist)
+motion = Twist()
+def callback(msg):
+    position = msg.pose.position
+        if position.x < 1:
+            motion.linear.x = +0.5
+        if position.x > 2:
+            motion.linear.x = -0.5
+    cmd.publish(motion)
+
+rospy.init_node("rostuto1")
+rospy.Subscriber("/atrv/pose", PoseStamped, callback)
+rospy.spin()
+
+'''
