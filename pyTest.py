5f43e603ee56b865ddd848f24dc5eb3ae5e16922
ramanv <ramanv@D-69-91-156-146.dhcp4.washington.edu>
2014-12-10 16:58:57 -0800

    Added movebase to pyTest

--- pyTest.py
+++ pyTest.py
@@ -3,6 +3,25 @@ from __future__ import division
 import rospy
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
+
+
+cmd = rospy.Publisher("/atrv/motion", Twist)
+motion = Twist()
+def callback(msg):
+    position = msg.pose.position
+        if position.x < 1:
+            motion.linear.x = +0.5
+        if position.x > 2:
+            motion.linear.x = -0.5
+    cmd.publish(motion)
+
+rospy.init_node("rostuto1")
+rospy.Subscriber("/atrv/pose", PoseStamped, callback)
+rospy.spin() # this will block untill you hit Ctrl+C
+
+
 file = open('test.txt', 'w')
 def main():
 	rospy.init_node('sub_pcl');
@@ -20,8 +39,7 @@ class Processor(object):
 
 
 	def callback(self, data):
-		"""Computes the average point in a point cloud and saves timing info.
-		"""
+		"""Computes the average point in a point cloud and saves timing info."""
 		points = pc2.read_points(data, field_names=['x', 'y', 'z'],
 								skip_nans=True)
 		num_points = 0
@@ -44,6 +62,7 @@ class Processor(object):
 		self._time_taken += current_time - self._prev_time
 		self._prev_time = current_time
 		self._num_calls += 1
+
 		rospy.loginfo('x: {}, y: {}, z: {}, time/point: {}s'.format(
 			avg_x, avg_y, avg_z, self._time_taken.to_sec() / self._num_calls))
 
@@ -51,6 +70,6 @@ class Processor(object):
 		distance = (avg_x**2 + avg_y**2 + avg_z**2)**.5
 		file.write(str(distance))
 		file.write("\n")
-
+		
 if __name__ == '__main__':
 	main()
